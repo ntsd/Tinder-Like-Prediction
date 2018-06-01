e973b7e6e43fdacd71c5c3601c7c33e2a9eaa1af
Jiravat Boonkumnerd <jo06942@gmail.com>
2018-06-01 23:49:27 +0700

    Replace face feature

--- featureExtraction/Feature.py
+++ featureExtraction/Feature.py
@@ -1,10 +1,17 @@
 import dlib
+import math
+import cv2
+import face_recognition_models
+import face_recognition
+import numpy as np
+from skimage.feature import hog
+
 
 class Feature:
 	def __init__(self, predictor_path='shape_predictor_68_face_landmarks.dat'):
 		self.detector = dlib.get_frontal_face_detector()
 		self.predictor = dlib.shape_predictor(predictor_path)
-
+		
 	def getFaceRects(self, img):
 		dets = self.detector(img, 1)
 		return dets
@@ -14,20 +21,91 @@ class Feature:
 		return max(rects, key=bigest)
 
 	def getFaceShape(self, img, rect):
+		# Mouth 0-16
+		# Right eyebrow 17-21 
+		# Left eyebrow 22-26
+		# Nose 27-30
+		# Nose Hole 31-35
+		# Right eye 36-41
+		# Left eye 42-47
+		# Jaw 48-67
 		shape = self.predictor(img, rect)
-		return shape.parts()
+		return shape.parts() 
 
 	def show(self):
 		pass
 
-	def getFeature(self, path):
+	def angle(self, vector1, vector2):
+		length1 = math.sqrt(vector1[0] * vector1[0] + vector1[1] * vector1[1])
+		length2 = math.sqrt(vector2[0] * vector2[0] + vector2[1] * vector2[1])
+		return math.acos((vector1[0] * vector2[0] + vector1[1] * vector2[1])/ (length1 * length2))
+
+	def shape_to_directions_feature(self, shape):
+		directions = []
+		shapes = [shape[0:17],
+					shape[17:22],
+					shape[22:27],
+					shape[27:31],
+					shape[31:36],
+					shape[36:42],
+					shape[42:48],
+					shape[48:]]
+		for shape in shapes:
+			ox, oy = shape[0]
+			for x, y in shape[1:]:
+				angle = self.angle([x, y], [ox, oy])
+				directions.append(angle)
+				ox, oy = x, y
+		return directions
+
+	def shape_to_distance_feature(self, shape):
+		distances = []
+		ox, oy = shape[0]
+		for x, y in shape[1:]:
+			dis = math.sqrt((ox-x)**2 + (oy-y)**2)
+			distances.append(dis)
+			ox, oy = x, y
+		return distances
+
+	def norm_size(self, img_path, points):
+		img = cv2.imread(img_path)
+		height, width, _ = img.shape
+		# print(height, width)
+		return [ [point.x/width, point.y/height] for point in points]
+
+	def hog_feature(self, img, faceRect):
+		# print(faceRect.tl_corner().x,faceRect.br_corner().x,faceRect.tl_corner().y,faceRect.br_corner().y)
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		# print(img.shape)
+		crop_img = img[faceRect.tl_corner().y:faceRect.br_corner().y,faceRect.tl_corner().x:faceRect.br_corner().x]
+		# print(crop_img.shape)
+		img = cv2.resize(crop_img, (128, 128))
+		scale_cell = (8, 8) #(img.shape[0]//100, img.shape[1]//100)
+		fHOG1 = hog(img, orientations=8, pixels_per_cell=scale_cell)
+		return fHOG1
+
+	def getFeatureOld(self, path):
 		img = dlib.load_rgb_image(path)
 		rect_faces = self.getFaceRects(img)
 		if not rect_faces:
 			return None
-		rectFace = self.getBigestFaceRect(rect_faces)
-		faceShape = self.getFaceShape(img, rectFace)
-		return faceShape
+		rectFace = self.getBigestFaceRect(rect_faces) # get bigest face
+		faceShape = self.getFaceShape(img, rectFace) # get face shape
+		normShape = self.norm_size(path, faceShape)
+		# Features
+		features = self.shape_to_directions_feature(normShape)
+		features = self.shape_to_distance_feature(normShape)
+		features = list(self.hog_feature(img, rectFace))
+		features = list(self.hog_feature(img, rectFace)) + self.shape_to_directions_feature(normShape) + self.shape_to_distance_feature(normShape)
+		return features
+
+	def getFeature(self, path):
+		img = dlib.load_rgb_image(path)
+		face_bounding_boxes = face_recognition.face_locations(img)
+		if len(face_bounding_boxes) == 0: # if face not found use np zeroes shape 128 
+			return np.zeros(shape=(128))
+		features = face_recognition.face_encodings(img, known_face_locations=face_bounding_boxes)[0]
+		return features
 
 if __name__ == '__main__':
 	f = Feature()
--- featureExtraction/preprocess.py
+++ featureExtraction/preprocess.py
@@ -4,43 +4,49 @@ from Feature import Feature
 from sklearn.preprocessing import normalize
 from loadFacePath import loadFaceData
 from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
-
-def norm_size(img_path, points):
-    img = cv2.imread(img_path)
-    height, width, _ = img.shape
-    print(height, width)
-    return [ (point.x/width, point.y/height) for point in points]
-
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.svm import LinearSVC
 
 if __name__ == "__main__":
     f = Feature()
     paths, classes = loadFaceData('face.csv')
     X = []
     y = []
+    # add face not found class
+    # for i in range(5):
+    #     X.append(np.zeros(shape=(128)))
+    #     y.append('Face_Not_Found')
     for index, path in enumerate(paths):
-        ar = f.getFeature(path)
-        if ar == None:
-            continue
-        norm_arr = norm_size(path, ar)
-        X.append(norm_arr)
-        y.append(classes[index])
-        if index == 10:
+        if index == 82:
             break
+        ar = f.getFeature(path)
+        print(index, path)
+        if ar.all() == 0:
+            continue
+            X.append(ar)
+            y.append('Face_Not_Found')
+        X.append(ar)
+        y.append(classes[index])
     # print(y)
     # print(X)
     X = np.array(X)
     y = np.array(y)
     print(X.shape)
     print(X)
-    print(X.reshape(11, 136))
+    print(y)
     X_train_data, X_test_data, y_train_data, y_test_data = train_test_split(X, y, test_size=0.3, stratify=y)
     nearestCentroid = NearestCentroid()
     nearestCentroid.fit(X_train_data, y_train_data)
 
     predict_y = nearestCentroid.predict(X_test_data)
-    f1Score = f1_score(y_test_data, predict_y, average='macro')
+    acc = accuracy_score(y_test_data, predict_y)
 
-    print(f1Score)
+    print(acc)
+
+    # Feature Importance
+    # from sklearn.ensemble import RandomForestClassifier
+    # rf = RandomForestClassifier(random_state=42).fit(X, y)
+    # print(rf.feature_importances_)
     
