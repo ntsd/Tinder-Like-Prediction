1d5b473daf45bff22d7209ff438899ba0a1ddd3d
Jiravat Boonkumnerd <jo06942@gmail.com>
2018-06-03 02:14:39 +0700

    Added color feature

--- featureExtraction/Feature.py
+++ featureExtraction/Feature.py
@@ -1,7 +1,7 @@
 import face_recognition_models
 import face_recognition
 import numpy as np
-
+import cv2
 
 class Feature:
 	"""
@@ -25,6 +25,16 @@ class Feature:
 		bigest = lambda rect: rect[2]-rect[0] * rect[1]-rect[3] # (top, right, bottom, left)
 		return max(rects, key=bigest)
 
+	def colorFeature(self, img):
+		hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+		# cv2.imshow('test', hsv)
+		# cv2.waitKey(0)
+		chans = cv2.split(hsv)  # take the S channel 
+		S = chans[1]
+		hist2 = cv2.calcHist([S], [0], None, [8], [0, 256])
+		hist2 = hist2/hist2.sum()
+		return hist2[:,0]
+
 	def getFeature(self, path):
 		"""
 		Input: Image path
@@ -36,6 +46,9 @@ class Feature:
 			return np.zeros(shape=(128))
 		bigest_face_rect= self.getBigestFaceRect(face_rects)
 		features = face_recognition.face_encodings(img, known_face_locations=[bigest_face_rect])[0]
+
+		faceImage = img[bigest_face_rect[0]:bigest_face_rect[2],bigest_face_rect[3]:bigest_face_rect[1]]
+		features = np.append(features, self.colorFeature(faceImage))
 		return features
 
 if __name__ == '__main__':
